642a20260f9f7103820677d4cd70552d87eea9c3
Coralie2798 <coralie.bodiguel@gmail.com>
2023-03-27 13:42:50 +0200

    exo complet sans correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes:Routes=[
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
